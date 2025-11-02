2e62f0d8aa50a109de03ffeb0f5c8d5e26278b73
rogerfernandesdeveloper <rogerfernandes.developer@gmail.com>
2025-11-02 17:16:20 -0300

    update profile

--- components/AsideRight.tsx
+++ components/AsideRight.tsx
@@ -8,20 +8,42 @@ export default function AsideRight(){
 
             </Image>
           </figure>
-          <table className="w-10/12 border-2 p-6 border-gray-300 space-y-8">
-            <tbody className="w-full">
-                <tr className="w-full">
-                    <td className="text-center font-bold pr-2">Name:</td>
-                    <td className="text-center pr-2">Roger Fernandes</td>
+        <table className="w-10/12 border-separate border-spacing-y-4 bg-gray-700 text-white border-2 border-gray-300">
+            <tbody>
+                <tr className="flex">
+                    <td className="font-bold px-2">Name:</td>
+                    <td>Roger Fernandes</td>
                 </tr>
             </tbody>
-            <tbody className="w-full">
+            <tbody>
+                <tr className="flex">
+                    <td className="font-bold px-2">Age:</td>
+                    <td>33 years old</td>
+                </tr>
+            </tbody>
+            <tbody>
                 <tr>
-                    <td className="text-center font-bold pr-2">Age:</td>
-                    <td className="text-center pr-2">33 years old</td>
+                    <td className="font-bold px-2">Education:</td>
                 </tr>
             </tbody>
-          </table>
+            <tbody>
+                 <tr>
+                    <td className="px-2">Computer Science 2012-2016 <i>UNIP</i></td>
+                </tr>
+            </tbody>
+            <tbody>
+                <tr>
+                    <td className="px-2">Software engineering 2025-2029 <i>GranFaculdade</i></td>
+                </tr>
+            </tbody>
+            <tbody>
+                <tr className="flex">
+                    <td className="font-bold px-2">Location:</td>
+                    <td>Promissão/SP Brazil</td>
+                </tr>
+            </tbody>
+        </table>
+
         </aside>
     )
 }
